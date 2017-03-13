1a6f494335323b646e57c50b16505a7f0d4f831b
guozhong <guozhong@qeeniao.com>
2017-03-13 09:44:19 +0800

    2 修改data类 使其支持空构造方法

--- src/main/java/org/gosky/domain/User.kt
+++ src/main/java/org/gosky/domain/User.kt
@@ -3,5 +3,5 @@ package org.gosky.domain
 /**
  * Created by guozhong on 17/3/9.
  */
-data class User(var id: Long?, var name: String?, var age: Int?)
+data class User(var id: Long? = null, var name: String? = null, var age: Int? = null)
 
