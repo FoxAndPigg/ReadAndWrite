6d6029c27a442368df0456433bd738834498f909
guozhong <guozhong@qeeniao.com>
2017-03-13 10:14:13 +0800

    3 swagger2的使用

--- src/main/java/org/gosky/web/UserController.java
+++ src/main/java/org/gosky/web/UserController.java
@@ -13,6 +13,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 /**
  * Created by guozhong on 17/3/9.
  */
@@ -21,11 +25,17 @@ import java.util.Map;
 public class UserController {
     static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
 
+    @ApiOperation(value = "获取用户信息列表", notes = "notes")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<User> getUserList() {
         return new ArrayList<User>(users.values());
     }
 
+    @ApiOperation(value = "修改用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户详情实体user", required = true, dataType = "user")
+    })
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public String putUser(@PathVariable Long id, @ModelAttribute User user) {
         User u = users.get(id);
@@ -41,7 +51,6 @@ public class UserController {
         users.put(user.getId(), user);
         return "success";
     }
-
 
 
 }
